8c22dbe81129f69851fb95e3c8127bc6de647dc2
etrnalembers <122410731+etrnalembers@users.noreply.github.com>
2026-04-02 09:15:09 -0600

    node.js Bypass for devtools
    cash-bowen already knows this project is compatible with Node, so this was rewritten for an incorporated node.js cookie puller that shouldn't require the devtools console open. Just use install.html!

--- soloDecoder.js
+++ soloDecoder.js
@@ -1,12 +1,14 @@
 const frame = window.frames['IframeLesson'].contentWindow;
 let cookie = frame.Nida.Cookies.get("assessment");
+
 function decode(input) {
-	let encoded = input.split("$$").find(p => p.startsWith("pull:")).split(":")[1]
-	let encodedSplit = encoded.split(",")
+    let encoded = input.split("$$").find(p => p.startsWith("pull:")).split(":");
+    let encodedSplit = encoded.split(",");
     return encodedSplit.map((q, i) => {
         let parsed = parseInt(q.substr(q.length - 3, 3), 10);
         let value = parsed - 13 - i;
         return String.fromCharCode(value);
-    });
+    }).join("");
 }
-console.log(decode(cookie));
+
+alert("Decoded Assessment:\n\n" + decode(cookie));
